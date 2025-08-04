f425abfe2957786420a5f176057a6bd5ea9fed3e
Ta Cuong <cuongtcph51018@gmail.com>
2025-08-04 15:09:05 +0700

    code

--- src/services/LinkApi.js
+++ src/services/LinkApi.js
@@ -1,3 +1,3 @@
 
-export const API_BASE2 = 'https://92f8fa709052.ngrok-free.app';
-export const API_BASE = 'https://92f8fa709052.ngrok-free.app';
+export const API_BASE2 = 'https://6bb16771d823.ngrok-free.app';
+export const API_BASE = 'https://6bb16771d823.ngrok-free.app';
