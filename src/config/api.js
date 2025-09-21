a220ca00d6be437438d3df7c2cb336c5fcc2da31
DinhBao168 <baodgph51570@gmail.com>
2025-09-21 08:57:04 +0700

    thay link api

--- src/config/api.js
+++ src/config/api.js
@@ -6,7 +6,7 @@
 // const CORS_PROXY = 'https://corsproxy.io/?';
 
 // URL cơ sở API - sử dụng đường dẫn tương đối khi proxy được cấu hình
-const API_BASE = 'https://edf052525cbf.ngrok-free.app';
+const API_BASE = 'https://4292a232446b.ngrok-free.app';
 
 // Các điểm cuối
 const ENDPOINTS = {
